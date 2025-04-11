14116ab5bda8f49cb010efce8558a727432b918e
Murilo Mucci <murilomucci@gmail.com>
2025-04-11 09:35:59 -0300

    feat: criado metodo pra requisicao http

--- methods/ConsultaCep.java
+++ methods/ConsultaCep.java
@@ -1,10 +1,29 @@
 package buscaCEP.methods;
 
 import buscaCEP.modulo.Adress;
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class ConsultaCep {
     public Adress consultaCep(String cep){
-        Adress endereco = null;
-        return endereco;
+        URI link = URI.create("https://viacep.com.br/ws/" + cep + "/json");
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(link)
+                .build();
+        HttpResponse<String> response = null;
+        try {
+            response = HttpClient
+                    .newHttpClient()
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return new Gson().fromJson(response.body(), Adress.class);
     }
 }
