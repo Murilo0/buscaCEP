16420ee8dc388bcf5d70cd3885bc733f029e8b24
Murilo Mucci <murilomucci@gmail.com>
2025-04-15 12:09:29 -0300

    fix: erro de syntax corrigido e condicional adequada colocada.

--- Main.java
+++ Main.java
@@ -1,16 +1,27 @@
 package buscaCEP;
+import buscaCEP.methods.ConsultaCep;
+import buscaCEP.modulo.Adress;
+
+import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         String CEP = "";
+        ConsultaCep consultaCep = new ConsultaCep();
+        ArrayList adressesList = new ArrayList<Adress>();
 
-
-        System.out.println("Digite o seu CEP para que seja feita a busca do endereco: ");
-        CEP = scanner.nextLine();
-
-
-
+        while (!Objects.equals(CEP, "sair")){
+            System.out.println("Digite o seu CEP para que seja feita a busca do endereco: ");
+            CEP = scanner.nextLine();
+            if (Objects.equals(CEP, "sair")){
+                break;
+            }else{
+                Adress newAdress = consultaCep.consultaCep(CEP);
+                adressesList.add(newAdress);
+            }
+        }
     }
 }
--- methods/ConsultaCep.java
+++ methods/ConsultaCep.java
@@ -2,6 +2,7 @@ package buscaCEP.methods;
 
 import buscaCEP.modulo.Adress;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import java.io.IOException;
 import java.net.URI;
@@ -10,20 +11,28 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class ConsultaCep {
-    public Adress consultaCep(String cep){
-        URI link = URI.create("https://viacep.com.br/ws/" + cep + "/json");
+    public Adress consultaCep(String cep) {
+        URI link = URI.create("https://viacep.com.br/ws/" + cep + "/json/");
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(link)
                 .build();
-        HttpResponse<String> response = null;
         try {
-            response = HttpClient
-                    .newHttpClient()
-                    .send(request, HttpResponse.BodyHandlers.ofString());
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            String json = response.body();
+            System.out.println("JSON recebido: " + json); // <-- debug útil
+
+            // Verifica se o JSON retornado indica erro (como {"erro": true})
+            if (json.contains("\"erro\": true")) {
+                throw new RuntimeException("CEP não encontrado.");
+            }
+
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            return gson.fromJson(json, Adress.class);
+
         } catch (IOException | InterruptedException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Erro ao consultar o CEP", e);
         }
-        return new Gson().fromJson(response.body(), Adress.class);
     }
 }
