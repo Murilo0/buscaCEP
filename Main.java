887ed423e0fbde2f5c6abb535091491d5c44629e
Murilo Mucci <murilomucci@gmail.com>
2025-04-24 11:47:21 -0300

    feat: agora com um FileWritter para gravarmos um arquivo JSON com os CEPS pesquisados
    e isso
    sair
    exit

--- Main.java
+++ Main.java
@@ -1,22 +1,31 @@
 package buscaCEP;
 import buscaCEP.methods.ConsultaCep;
 import buscaCEP.modulo.Adress;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
         String CEP = "";
         ConsultaCep consultaCep = new ConsultaCep();
-        ArrayList adressesList = new ArrayList<Adress>();
+        ArrayList<Adress> adressesList = new ArrayList<Adress>();
 
         while (!Objects.equals(CEP, "sair")){
             System.out.println("Digite o seu CEP para que seja feita a busca do endereco: ");
             CEP = scanner.nextLine();
             if (Objects.equals(CEP, "sair")){
+                FileWriter escrita = new FileWriter("enderecos.json");
+                escrita.write(gson.toJson(adressesList));
+                escrita.close();
+                scanner.close();
                 break;
             }else{
                 Adress newAdress = consultaCep.consultaCep(CEP);
